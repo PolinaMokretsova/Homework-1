bf63bca1b5c1678785a4edf454d33c6c845cf215
PolinaMokretsova <pmokretsova@mail.ru>
2022-06-15 20:19:28 +0500

    edit fixture

--- test_homework.py
+++ test_homework.py
@@ -3,7 +3,7 @@ from selene.support.shared import browser
 from selene import be, have
 import pytest
 
-@pytest.fixture()
+@pytest.fixture(scope='session')
 def browser_size():
     browser.open('https://duckduckgo.com/').driver.maximize_window()
 
@@ -11,16 +11,16 @@ def browser_size():
 def test_open(browser_size):
    browser.open('https://duckduckgo.com/')
 
-def test_search():
+def test_search(browser_size):
    browser.element('[name="q"]').should(be.blank).type('Selene - User-oriented Web UI browser tests in Python').press_enter()
 
-def test_text():
+def test_text(browser_size):
    browser.element('[id="links_wrapper"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
 
 #негативный тест на поиск, чтобы он не выдавал результатов
 
-def test_text_negative():
+def test_text_negative(browser_size):
    browser.element('[id="links_wrapper"]').should(have.no.text('Apple'))
 
 
